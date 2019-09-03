c5d1da138e06e6e0de2740241f81f05f50a2f31f
Jianbin <jibeex@gmail.com>
2019-09-03 12:01:59 +0200

    Recreate server target

---
+++ SBTUITestTunnel_Server/SBTUITestTunnel_Server.h
@@ -0,0 +1,19 @@
+//
+//  SBTUITestTunnel_Server.h
+//  SBTUITestTunnel_Server
+//
+//  Created by Jianbin LIN on 03/09/2019.
+//  Copyright © 2019 Qonto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBTUITestTunnel_Server.
+FOUNDATION_EXPORT double SBTUITestTunnel_ServerVersionNumber;
+
+//! Project version string for SBTUITestTunnel_Server.
+FOUNDATION_EXPORT const unsigned char SBTUITestTunnel_ServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBTUITestTunnel_Server/PublicHeader.h>
+
+
