ec6afc4f2aecb5d8fa67da1e0238d8cb3fb06faf
Jianbin <jibeex@gmail.com>
2019-09-03 12:02:16 +0200

    Recreate tunnel server target

--- SBTUITestTunnel_Server/SBTUITestTunnel_Server.h
+++ SBTUITestTunnel_Server/SBTUITestTunnel_Server.h
@@ -17,3 +17,23 @@ FOUNDATION_EXPORT const unsigned char SBTUITestTunnel_ServerVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SBTUITestTunnel_Server/PublicHeader.h>
 
 
+#import <SBTUITestTunnel_Server/SBTUITestTunnelServer.h>
+#import <SBTUITestTunnel_Server/NSURLSession+HTTPBodyFix.h>
+#import <SBTUITestTunnel_Server/SBTProxyURLProtocol.h>
+#import <SBTUITestTunnel_Server/NSURLSessionConfiguration+SBTUITestTunnel.h>
+#import <SBTUITestTunnel_Server/SBTAnyViewControllerPreviewing.h>
+#import <SBTUITestTunnel_Server/UIView+Extensions.h>
+#import <SBTUITestTunnel_Server/UITextField+DisableAutocomplete.h>
+#import <SBTUITestTunnel_Server/UIViewController+SBTUITestTunnel.h>
+#import <SBTUITestTunnel_Server/NSData+SHA1.h>
+
+#import <SBTUITestTunnel_Server/NSData+gzip.h>
+#import <SBTUITestTunnel_Server/SBTSwizzleHelpers.h>
+#import <SBTUITestTunnel_Server/NSURLRequest+HTTPBodyFix.h>
+#import <SBTUITestTunnel_Server/SBTMonitoredNetworkRequest.h>
+#import <SBTUITestTunnel_Server/SBTUITestTunnel.h>
+#import <SBTUITestTunnel_Server/SBTStubResponse.h>
+#import <SBTUITestTunnel_Server/NSURLRequest+SBTUITestTunnelMatch.h>
+#import <SBTUITestTunnel_Server/SBTRewrite.h>
+#import <SBTUITestTunnel_Server/SBTRequestMatch.h>
+#import <SBTUITestTunnel_Server/NSString+SwiftDemangle.h>
